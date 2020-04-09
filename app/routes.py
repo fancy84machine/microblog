b8c21674d993ecaf6631df33e484d959e3daf200
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-09 16:09:40 -0400

    his chapter 4 with requirements installed

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -26,4 +27,4 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html',  title='Sign In', form=form)
