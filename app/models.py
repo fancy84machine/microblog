761c21ee88b95390dadc5e1402473c97c7024676
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-09 16:33:19 -0400

    fixed before first test

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-class User(db.Model, db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -24,7 +24,7 @@ class User(db.Model, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-    
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
