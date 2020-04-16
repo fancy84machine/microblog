daeb01517dc535435b92ebe2c6a7ac7acd65a843
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-16 13:53:18 -0400

    adding removing followers

--- app/models.py
+++ app/models.py
@@ -40,6 +40,18 @@ class User(UserMixin, db.Model):
         digest = md5 (self.email.lower().encode ('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avator/{}?d=identicon&s={}'.format (digest, size)
 
+    def follow (self, user):
+        if not self.is_following(user):
+            self.followed.append (user)
+        
+    def unfollow (self, user):
+        if self.is_following (user):
+            self.followed.remove (user)
+        
+    def is_following (self, user):
+        return self.followed.filter (followers.c.followed_id==user.id).count () > 0
+
+        
 
 
 @login.user_loader
